ffb60e5d9916d10c9d02f4214f82710c92c08933
saiful10400 <saifulislam10400i5@gmail.com>
2023-12-11 10:31:30 +0600

    all necessary files has added.

---
+++ index.js
@@ -0,0 +1,49 @@
+const express=require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express()
+const port=process.env.port || 5000
+require("dotenv").config()
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.qe6izo7.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+ 
+ 
+    app.get("/",async(req,res)=>{
+        res.send(`this server is running on ${port} port.`)
+    })
+
+
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port,()=>{
+    console.log(`this server is running on https://localhost:${port}`)
+})
